973a1fcf7e896dbd176c8ee4c81702abffa421ad
PhongBeetech <phong.pv@beetechsoft.vn>
2024-06-13 21:32:01 +0700

    kill audio when terminated

--- lib/app.dart
+++ lib/app.dart
@@ -7,13 +7,38 @@ import 'package:music_app/core/core.dart';
 import 'package:music_app/features/presentation/blocs/blocs.dart';
 import 'package:music_app/l10n/l10n.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:music_app/service/audio_service/app_audio_services.dart';
 
 import 'features/presentation/ui/common_widgets/loading/custom_loading.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.detached) {
+      GetIt.I.get<AppAudioServices>().dispose();
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
